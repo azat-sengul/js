8377418288f2daac63b7221028cce0a349d7a253
azat-sengul <azat.sengul@hotmail.com>
2024-01-12 18:01:44 +0300

    Uygulamalar

---
+++ fonksiyon.js
@@ -0,0 +1,49 @@
+function Linking()
+{
+    alert("Hello js link....")
+}
+
+function Kare(x) {
+
+    return x * x;
+    
+}
+
+function Kup(y) {
+    
+    return y * y * y;
+}
+
+function Topla(a, b) {
+    return a +  b;
+}
+
+function Carpim(a, b) {
+    return a * b;
+}
+
+function Yaz(deger) {
+
+    document.write(deger + "<br>");
+    
+}
+
+function Merhaba() {
+    
+    document.write( i + ". " + "Merhaba" + "<br>" );
+
+}
+
+function Alan(r) 
+{
+    var pi = 3.14;
+    var alan = pi * r * r;
+    return alan;
+}
+
+function Cevre(r)
+{
+    var pi = 3.14;
+    var cevre = 2 * pi * r;
+    return cevre;
+}
